38d3ab858579b2d2cac87d00033d56622507f402
Allen Yeung <ayeung@aptana.com>
2010-06-10 11:25:03 -0700

    Fix #500: Added check for installed bundle before prompting user to install as new bundle

--- commands/install_bundle.rb
+++ commands/install_bundle.rb
@@ -5,9 +5,9 @@ require 'fileutils'
 # HEY! Want your Ruble included here? Send us a pull request on github!
 RubleInfo = Struct.new(:display_name, :directory_name, :repository)
 INSTALLABLE_RUBLES = [
-  RubleInfo.new("GitHub",        "github",       "git://github.com/aptana/github.ruble.git"),
-  RubleInfo.new("Shell Script",  "shell-script", "git://github.com/aptana/shell-script.ruble.git"),
-  RubleInfo.new("WebFont",       "WebFont",      "git://github.com/aptana/WebFont.ruble.git")
+  RubleInfo.new("GitHub",        "github.ruble",       "git://github.com/aptana/github.ruble.git"),
+  RubleInfo.new("Shell Script",  "shell-script.ruble", "git://github.com/aptana/shell-script.ruble.git"),
+  RubleInfo.new("WebFont",       "WebFont.ruble",      "git://github.com/aptana/WebFont.ruble.git")
 ]
 
 # This asks the user which of the known bundles they would like to install
@@ -16,17 +16,34 @@ command "Install Bundle" do |cmd|
   cmd.output = :show_as_tooltip
   cmd.invoke do |context|
     # Ask user which of the pre-installed bundles to grab!
+    bundles_dir = Ruble::BundleManager.manager.user_bundles_path
+    app_bundles_dir = Ruble::BundleManager.manager.application_bundles_path
+
     options = {}
-    options[:items] = INSTALLABLE_RUBLES.map {|bundle| bundle.display_name }
-    chosen = Ruble::UI.request_item(options)
+    items = []
+    INSTALLABLE_RUBLES.each do |bundle| 
+      if !File.exists?(File.join(app_bundles_dir, bundle.directory_name)) && !File.exists?(File.join(bundles_dir, bundle.directory_name))
+        items << bundle.display_name 
+      end
+    end
+    
+    options[:items] = items
+    if items.length == 1
+      options[:button1]= "Install"
+      options[:title] = "Install Bundle"
+      options[:prompt] = "Install #{items.first} Bundle?"
+      Ruble::UI.request_confirmation(options) ? chosen = items.first : chosen = nil
+    else
+      options[:title] = "Select Bundle to Install"
+      chosen = Ruble::UI.request_item(options)
+    end
     context.exit_show_tooltip("No bundle selected") if chosen.nil?
       
     ruble_info = INSTALLABLE_RUBLES.select {|ruble_info| ruble_info.display_name == chosen}.first
     context.exit_discard if ruble_info.nil?
       
-    bundles_dir = Ruble::BundleManager.manager.user_bundles_path
     FileUtils.makedirs(bundles_dir)
-
+    
     Dir.chdir(bundles_dir)
     # If Ruble already exists, we should punt
     if File.exists?(ruble_info.directory_name)
