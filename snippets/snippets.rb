c99faebd4ce2a810f71178c98f2b92abcc2fb3c4
sgtcoolguy <cwilliams@aptana.com>
2010-11-29 13:14:32 -0500

    fix indentation, add snippet for project_template

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -2,43 +2,43 @@ require 'ruble'
 
 with_defaults :scope => 'source.ruby' do
 
-# Snippet to generate a snippet
-snippet 'snippet' do |s|
-  s.trigger = 'sn'
-  s.expansion = 
+  # Snippet to generate a snippet
+  snippet 'snippet' do |s|
+    s.trigger = 'sn'
+    s.expansion = 
 "snippet '${1:name}' do |s|
   s.trigger = '${2:trigger}'
   s.scope = '${3:source.ruby}'
   s.expansion = \"${4:example}\"
 end"
-end
+  end
 
-# Snippet to repeatedly insert menu command items
-snippet 'm.command' do |s|
-  s.trigger = 'm.'
-  s.expansion = "m.command '${1:command_or_snippet_name}'
+  # Snippet to repeatedly insert menu command items
+  snippet 'm.command' do |s|
+    s.trigger = 'm.'
+    s.expansion = "m.command '${1:command_or_snippet_name}'
 m.$0"
-end
+  end
 
-# Insert a menu separator
-snippet 'm.separator' do |s|
-  s.trigger = 'm.'
-  s.expansion = "m.separator
+  # Insert a menu separator
+  snippet 'm.separator' do |s|
+    s.trigger = 'm.'
+    s.expansion = "m.separator
 m.$0"
-end
+  end
 
 # Generate a sub-menu
-snippet 'm.menu' do |s|
-  s.trigger = 'm.'
-  s.expansion = "m.menu '${1:submenu}' do |m|
+  snippet 'm.menu' do |s|
+    s.trigger = 'm.'
+    s.expansion = "m.menu '${1:submenu}' do |m|
   m.$0
 end"
-end
+  end
 
-# Snippet to generate a command
-snippet 'command' do |s|
-  s.trigger = 'co'
-  s.expansion = 
+  # Snippet to generate a command
+  snippet 'command' do |s|
+    s.trigger = 'co'
+    s.expansion = 
 "require 'ruble'
 
 command '${1:name}' do |cmd|
@@ -52,17 +52,17 @@ command '${1:name}' do |cmd|
     nil
   end
 end"  
-end
-
-# Snippet to generate a bundle
-command 'bundle' do |s|
-  s.trigger = 'bu'
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.invoke do
-    org = ENV["TM_ORGANIZATION_NAME"] || 'Example.org'
-    full_name = ENV['TM_FULLNAME'] || ENV['USER'] || "John Doe"
-    user = ENV['USER'] || 'user'
+  end
+  
+  # Snippet to generate a bundle
+  command 'bundle' do |s|
+    s.trigger = 'bu'
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.invoke do
+      org = ENV["TM_ORGANIZATION_NAME"] || 'Example.org'
+      full_name = ENV['TM_FULLNAME'] || ENV['USER'] || "John Doe"
+      user = ENV['USER'] || 'user'
 "require 'ruble'
 
 bundle '${1:Bundle name}' do |bundle|
@@ -81,13 +81,13 @@ END
     m.$0
   end
 end"
+    end
   end
-end
 
-# Snippet to generate a content assistant
-snippet 'content_assist' do |s|
-  s.trigger = 'ca'
-  s.expansion = 
+  # Snippet to generate a content assistant
+  snippet 'content_assist' do |s|
+    s.trigger = 'ca'
+    s.expansion = 
 "require 'ruble'
 
 content_assist '${1:name}' do |ca|
@@ -101,27 +101,36 @@ content_assist '${1:name}' do |ca|
     [${3:'example', 'values'}].inspect
   end
 end"  
-end
-
-# Snippet to generate with_defaults
-snippet 'with_defaults' do |s|
-  s.trigger = 'wi'
-  s.expansion = 
+  end
+  
+  # Snippet to generate with_defaults
+  snippet 'with_defaults' do |s|
+    s.trigger = 'wi'
+    s.expansion = 
 "with_defaults :${1:scope} => '${2:source.ruby}' do
   $0
 end"  
-end
-
-snippet 'env' do |s|
-  s.trigger = 'env'
-  s.expansion=
+  end
+  
+  snippet 'env' do |s|
+    s.trigger = 'env'
+    s.expansion=
 "env '${1:source.ruby}' do |e|
   e['${2:TM_COMMENT_START}'] = '${3:# }'
 end"
-end
+  end
 
   snippet 'smart_typing_pairs' do |s|
     s.trigger = 'stp'
     s.expansion = "smart_typing_pairs['${1:source.ruby}'] = ['${2:\"}', '$2']"
+  end
+  
+  snippet 'project_template' do |s|
+    s.trigger = 'pt'
+    s.expansion = "project_template '${1:name}' do |t|
+  t.type = :${2:name}
+  t.location = '${3:relative_filename.zip}'
+  t.description = '${4:description}'
+end"
   end
 end
